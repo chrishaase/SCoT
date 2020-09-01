5d2b6d27e459cd794e9a292d86be35855af65236
ch <haase.mail@web.de>
2020-09-01 11:51:58 +0200

    scotti pd3

--- src_vue/static/vue.js
+++ src_vue/static/vue.js
@@ -32,9 +32,8 @@ app = new Vue({
 		// excluded 
 		graph_type_keys: {"SCoTTi & Chinese Whispers (CW)": "max_per_slice",
 						"SCoT & CW": "max_across_slices",				
-						"SCoTTi-Learn (SCoTTi 1-2 & CW & SCoTTi 3)":"learn-2-base",
-						"SCoTTi+ & CW":"scottiplus"	
-									
+						"SCoTTi-Learn (SCoTTi 1-2 & CW & SCoTTi 3)": "learn-2-base",
+						"SCoTTi+ & CW": "scottiplus"
 					},
 		// limits the size of clusters for context-information-search
 		// queries larger than 50 usually take too long (can vary accoring to db optimization)
